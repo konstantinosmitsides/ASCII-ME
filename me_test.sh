acadcc78dea2065f187523bc16e55d823a20b91f
Konstantinos Mitsides <konstantinos.mitsides23@imperial.ac.uk>
2024-07-30 23:08:15 +0100

    automate experiments pipeline

---
+++ me_test.sh
@@ -0,0 +1,35 @@
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=256 num_iterations=4000
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=256 num_iterations=4000
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=256 num_iterations=4000
+
+python main_me.py env=ant_uni seed=$RANDOM batch_size=256 num_iterations=4000
+python main_me.py env=ant_uni seed=$RANDOM batch_size=256 num_iterations=4000
+python main_me.py env=ant_uni seed=$RANDOM batch_size=256 num_iterations=4000
+
+python main_me.py env=ant_omni seed=$RANDOM batch_size=256 num_iterations=4000
+python main_me.py env=ant_omni seed=$RANDOM batch_size=256 num_iterations=4000
+python main_me.py env=ant_omni seed=$RANDOM batch_size=256 num_iterations=4000
+
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=512 num_iterations=2000
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=512 num_iterations=2000
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=512 num_iterations=2000
+
+python main_me.py env=ant_uni seed=$RANDOM batch_size=512 num_iterations=2000
+python main_me.py env=ant_uni seed=$RANDOM batch_size=512 num_iterations=2000
+python main_me.py env=ant_uni seed=$RANDOM batch_size=512 num_iterations=2000
+
+python main_me.py env=ant_omni seed=$RANDOM batch_size=512 num_iterations=2000
+python main_me.py env=ant_omni seed=$RANDOM batch_size=512 num_iterations=2000
+python main_me.py env=ant_omni seed=$RANDOM batch_size=512 num_iterations=2000
+
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=1024 num_iterations=1000
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=1024 num_iterations=1000
+python main_me.py env=walker2d_uni seed=$RANDOM batch_size=1024 num_iterations=1000
+
+python main_me.py env=ant_uni seed=$RANDOM batch_size=1024 num_iterations=1000
+python main_me.py env=ant_uni seed=$RANDOM batch_size=1024 num_iterations=1000
+python main_me.py env=ant_uni seed=$RANDOM batch_size=1024 num_iterations=1000
+
+python main_me.py env=ant_omni seed=$RANDOM batch_size=1024 num_iterations=1000
+python main_me.py env=ant_omni seed=$RANDOM batch_size=1024 num_iterations=1000
+python main_me.py env=ant_omni seed=$RANDOM batch_size=1024 num_iterations=1000
