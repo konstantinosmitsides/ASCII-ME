8ad4387f7981e855b996b6bbc30e8fcc28a18724
Konstantinos Mitsides <konstantinos.mitsides23@imperial.ac.uk>
2024-08-03 21:09:33 +0100

    changed sh script

--- me_mcpg_test_2.sh
+++ me_mcpg_test_2.sh
@@ -1,31 +1,9 @@
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=1 num_iterations=2000 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=1 num_iterations=2000
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=1 num_iterations=2000
+python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 learning_rate=1e-3 no_epochs=16
+python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 learning_rate=1e-3 no_epochs=16
+python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 learning_rate=1e-3 no_epochs=16
 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 
+python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 learning_rate=1e-3 no_epochs=8
+python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 learning_rate=1e-3 no_epochs=8
+python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 learning_rate=1e-3 no_epochs=8
 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=4 num_iterations=2000 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=4 num_iterations=2000 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=4 num_iterations=2000 
 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=8 num_iterations=2000 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=8 num_iterations=2000 
-python main_me_mcpg.py env=walker2d_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=8 num_iterations=2000
-
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=1 num_iterations=2000 
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=1 num_iterations=2000
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=1 num_iterations=2000
-
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=2 num_iterations=2000 
-
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=4 num_iterations=2000 
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=4 num_iterations=2000 
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=4 num_iterations=2000 
-
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=8 num_iterations=2000 
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=8 num_iterations=2000 
-python main_me_mcpg.py env=ant_uni seed=$RANDOM no_agents=512 buffer_add_batch_size=512 buffer_sample_batch_size=8 num_iterations=2000 
