6cf3434129eb28c36ac18d42e9a996907cd0f154
Konstantinos Mitsides <konstantinos.mitsides23@imperial.ac.uk>
2024-08-19 18:00:33 +0100

    .

---
+++ advanced_baseline_test.sh
@@ -0,0 +1,2 @@
+python main_me_mcpg_advanced_baseline.py env=ant_uni env.episode_length=1000 num_iterations=1000 buffer_sample_batch_size=8
+python main_me_mcpg_advanced_baseline.py env=walker2d_uni env.episode_length=1000 num_iterations=1000 buffer_sample_batch_size=8
