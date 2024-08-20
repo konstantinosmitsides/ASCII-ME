fea9ece0db564375574047ebb87bf71123b06d90
Konstantinos Mitsides <konstantinos.mitsides23@imperial.ac.uk>
2024-08-20 10:25:00 +0100

    .

--- advanced_baseline_test.sh
+++ advanced_baseline_test.sh
@@ -1,5 +1,5 @@
-python main_me_mcpg_advanced_baseline.py seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 buffer_sample_batch_size=2
-python main_me_mcpg_advanced_baseline.py seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 buffer_sample_batch_size=2
+#python main_me_mcpg_advanced_baseline.py seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 buffer_sample_batch_size=2
+#python main_me_mcpg_advanced_baseline.py seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 buffer_sample_batch_size=2
 
 python main_me_mcpg_advanced_baseline.py seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 buffer_sample_batch_size=8
 python main_me_mcpg_advanced_baseline.py seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 buffer_sample_batch_size=8
