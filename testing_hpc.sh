b91ca552eb73976cefca186e2581d06042662026
Konstantinos Mitsides <km2120@ic.ac.uk>
2024-11-01 19:03:40 +0000

    added the mcpg_me_emitter_before

--- testing_hpc.sh
+++ testing_hpc.sh
@@ -1,3 +1,10 @@
-  # MEMES
-python main.py algo=mcpg_me seed=$RANDOM env=anttrap_omni env.episode_length=250 num_iterations=2000 
+python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
+python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
+python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
+
+python main.py algo=mcpg_me seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 
+python main.py algo=mcpg_me seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 
+python main.py algo=mcpg_me seed=$RANDOM env=walekr2d_uni env.episode_length=1000 num_iterations=2000 
+
+
 
