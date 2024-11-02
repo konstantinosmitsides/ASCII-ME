69855cd2252d0c6773a781f35ad50a553226f8a4
Konstantinos Mitsides <km2120@ic.ac.uk>
2024-11-02 23:50:00 +0000

    checked out some visualizations

--- testing_hpc.sh
+++ testing_hpc.sh
@@ -1,10 +1,10 @@
-python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
-python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
-python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
+#python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
+#python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
+#python main.py algo=mcpg_me seed=$RANDOM env=hopper_uni env.episode_length=1000 num_iterations=2000 
 
 python main.py algo=mcpg_me seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 
 python main.py algo=mcpg_me seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 
-python main.py algo=mcpg_me seed=$RANDOM env=walekr2d_uni env.episode_length=1000 num_iterations=2000 
+python main.py algo=mcpg_me seed=$RANDOM env=walker2d_uni env.episode_length=1000 num_iterations=2000 
 
 
 
